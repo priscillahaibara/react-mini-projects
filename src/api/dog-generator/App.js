92c93e5a184575e4f9b74278af4ffed26fd54df6
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-19 20:56:17 -0300

    feat(dog-generator): add loading state

--- src/api/dog-generator/App.js
+++ src/api/dog-generator/App.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import './App.css'
+import "./App.css";
 
 export default function DogGenerator() {
   const [img, setImg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchImg = async function () {
+    setLoading(true);
+
     try {
       const response = await fetch("https://dog.ceo/api/breeds/image/random");
       const data = await response.json();
@@ -12,13 +15,18 @@ export default function DogGenerator() {
       setImg(data.message);
     } catch (err) {
       console.error("Error:", err);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="main-container">
-      <img className="img" src={img} />
-      <button className="button" onClick={fetchImg}>Get new dog</button>
+      <img className="img" alt="Random dog" src={img} />
+      <p className="loading">{loading && "Loading..."}</p>
+      <button className="button" onClick={fetchImg}>
+        Get new dog
+      </button>
     </div>
   );
 }
