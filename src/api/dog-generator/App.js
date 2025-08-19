5d997913de98fab5a0b69496db38d0276d28d370
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-19 20:38:36 -0300

    feat(dog-generator): add basic dog generator with fetch and image display

--- src/api/dog-generator/App.js
+++ src/api/dog-generator/App.js
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function DogGenerator() {
-    
+  const [img, setImg] = useState(null);
+
+  const fetchImg = async function () {
+    try {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const data = await response.json();
+      console.log(data);
+      setImg(data.message);
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
+  return (
+    <div>
+      <img src={img} />
+      <button onClick={fetchImg}>Get new dog</button>
+    </div>
+  );
 }
