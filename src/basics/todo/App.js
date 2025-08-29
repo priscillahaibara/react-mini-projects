c977190977a7575eda44ff92f327e8dcae8efab0
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-29 20:05:33 -0300

    feat(todo): add navigation bar component and ajust styles for consistent layout

--- src/basics/todo/App.js
+++ src/basics/todo/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import Header from './components/Header.js'
+import PageNav from "../../components/PageNav.js";
+import Header from "./components/Header.js";
 import Input from "./components/Input.js";
 import TaskList from "./components/TaskList.js";
 import Footer from "./components/Footer.js";
+import styles from './App.module.css'
 
 export default function Todo() {
   const [tasks, setTasks] = useState([]);
@@ -14,22 +16,27 @@ export default function Todo() {
 
   function handleDeleteTask(taskToDelete) {
     const filteredTasks = tasks.filter((task) => task.id !== taskToDelete);
-    setTasks(filteredTasks)
+    setTasks(filteredTasks);
   }
 
   function toggleTaskCompleted(taskCompleted) {
-    const updatedTasks = tasks.map((task) => task.id === taskCompleted ? {...task, completed: !task.completed} : task);
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskCompleted ? { ...task, completed: !task.completed } : task
+    );
     setTasks(updatedTasks);
   }
 
   return (
-    <>
+    <main className={styles.main}>
+      <PageNav />
       <Header />
       <Input onAddTask={handleAddTask} />
-      <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} onTaskCompleted={toggleTaskCompleted}/>
-      <Footer completedCount={completedCount}/>
-    </>
+      <TaskList
+        tasks={tasks}
+        onDeleteTask={handleDeleteTask}
+        onTaskCompleted={toggleTaskCompleted}
+      />
+      <Footer completedCount={completedCount} />
+    </main>
   );
 }
-
-
