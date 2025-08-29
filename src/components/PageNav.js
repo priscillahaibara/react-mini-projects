59faaa93b5c02415f7df0e6e5de0d05dd1ad7b6c
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-29 18:08:43 -0300

    feat(PageNav): add logo to navigation bar

--- src/components/PageNav.js
+++ src/components/PageNav.js
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
-      <div>Logo</div>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
