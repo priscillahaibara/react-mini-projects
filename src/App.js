3b4a46948901bc1888565f3344749a70c9015c33
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-29 13:49:54 -0300

    feat: add homepage layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Homepage from "./pages/Homepage.js";
 import Calculator from "./basics/calculator/App.js";
 import Counter from "./basics/counter/App.js";
 import DogGenerator from "./api/dog-generator/App.js";
 import Todo from "./basics/todo/App.js";
 
+
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1 style={{textAlign: 'center', margin: '2rem', fontSize: '3rem'}}>Welcome to React Mini Projects!</h1>} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/calculator" element={<Calculator />} />
         <Route path="/counter" element={<Counter />} />
         <Route path="/dog-generator" element={<DogGenerator/>}/>
         <Route path="/todo" element={<Todo />} />
       </Routes>
 
-      <div style={{height: '100vh'}}>
+      {/* <div style={{height: '100vh'}}>
         <ul style={{ display: 'flex', flexDirection: 'column', gap: '1rem', maxWidth: '60rem', margin: '0 auto', cursor: 'pointer' }}>
           <li style={{fontSize: '1.6rem'}}><Link to="/">Home</Link></li>
           <li style={{fontSize: '1.6rem'}}><Link to="/calculator">Calculator</Link></li> 
@@ -23,7 +25,7 @@ export default function App() {
           <li style={{fontSize: '1.6rem'}}><Link to="/dog-generator">Dog Generator</Link></li> 
           <li style={{fontSize: '1.6rem'}}><Link to="/todo">Todo</Link></li> 
         </ul>
-      </div>
+      </div> */}
     </Router>
   );
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import styles from "./Homepage.module.css";
+
+function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <section>
+        <h1>Welcome to React Mini Projects!</h1>
+        <h2>Here you can explore the projects I’ve created while learning and growing as a Front-End developer.</h2>
+      </section>
+    </main>
+  );
+}
+
+export default Homepage;
