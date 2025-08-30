136ed87b1e219e2d9cc97664eb4a9a4911d29d0f
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-30 14:29:25 -0300

    feat: add mobile navigation menu

--- src/components/PageNav.js
+++ src/components/PageNav.js
@@ -1,25 +1,39 @@
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import styles from "./PageNav.module.css";
+import { useState } from "react";
 
 function PageNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false)
+
   return (
     <nav className={styles.nav}>
       <Logo />
-      <ul>
+      <ul className={`${styles.list} ${isOpen ? styles.open : ''}`}>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink to='/about'>About</NavLink>
+          <NavLink to="/about">About</NavLink>
         </li>
         <li>
-          <NavLink to='/contact'>Contact</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
         </li>
         <li>
-          <NavLink to='/projects' className={styles.ctaLink}>Projects</NavLink>
+          <NavLink to="/projects" className={styles.ctaLink}>
+            Projects
+          </NavLink>
         </li>
       </ul>
+      <button className={styles.navToggle} onClick={toggleMenu}>
+        <ion-icon className={styles.iconMenu} name="menu-outline" style={{ opacity: isOpen ? 0 : 1 }} ></ion-icon>
+        <ion-icon className={styles.iconClose} name="close-outline" style={{ opacity: isOpen ? 1 : 0 }} ></ion-icon>
+      </button>
+
+      {isOpen && <div className={styles.overlay} onClick={closeMenu}></div>}
     </nav>
   );
 }
