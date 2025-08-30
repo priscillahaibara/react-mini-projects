2622c697c2bde3228aaf13010ca3bfb3ce95007c
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-30 08:31:48 -0300

    feat(about): add content for about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,26 @@
-import PageNav from '../components/PageNav'
-import styles from './About.module.css'
+import PageNav from "../components/PageNav";
+import styles from "./About.module.css";
+import img from "../assets/cat.jpg";
 
 function About() {
-    return (
-        <main className={styles.main}>
-            <PageNav />
-        </main>
-    )
+  return (
+    <main className={styles.main}>
+      <PageNav />
+      <section className={styles.section}>
+        <div>
+          <h1>About me</h1>
+          <p>
+            Hello! My name is Priscilla Haibara, I am a 4th year Software
+            Engineering student and a permanent resident of Canada. With a
+            unique combination of expertise in front-end development and a
+            background as a medical doctor, I strive to create impactful web
+            applications that blend technology and user-centric design.
+          </p>
+        </div>
+        <img src={img} alt="cat near a laptop" />
+      </section>
+    </main>
+  );
 }
 
-export default About
+export default About;
